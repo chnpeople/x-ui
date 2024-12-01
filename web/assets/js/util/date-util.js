5308b7a2e87f572f6b4b6f8153f514966421b4c3
chnpeople <a289815438@qq.com>
2024-12-01 15:20:40 +0800

    chang reset time

--- web/assets/js/util/date-util.js
+++ web/assets/js/util/date-util.js
@@ -10,7 +10,7 @@ const oneMonth = oneDay * 30; // 一个月的毫秒数
  * @param days 要减少的天数
  */
 Date.prototype.minusDays = function (days) {
-    return this.minusMillis(oneDay * days);
+  return this.minusMillis(oneDay * days);
 };
 
 /**
@@ -19,7 +19,7 @@ Date.prototype.minusDays = function (days) {
  * @param days 要增加的天数
  */
 Date.prototype.plusDays = function (days) {
-    return this.plusMillis(oneDay * days);
+  return this.plusMillis(oneDay * days);
 };
 
 /**
@@ -28,7 +28,7 @@ Date.prototype.plusDays = function (days) {
  * @param hours 要减少的小时数
  */
 Date.prototype.minusHours = function (hours) {
-    return this.minusMillis(oneHour * hours);
+  return this.minusMillis(oneHour * hours);
 };
 
 /**
@@ -37,7 +37,7 @@ Date.prototype.minusHours = function (hours) {
  * @param hours 要增加的小时数
  */
 Date.prototype.plusHours = function (hours) {
-    return this.plusMillis(oneHour * hours);
+  return this.plusMillis(oneHour * hours);
 };
 
 /**
@@ -46,7 +46,7 @@ Date.prototype.plusHours = function (hours) {
  * @param minutes 要减少的分钟数
  */
 Date.prototype.minusMinutes = function (minutes) {
-    return this.minusMillis(oneMinute * minutes);
+  return this.minusMillis(oneMinute * minutes);
 };
 
 /**
@@ -55,7 +55,7 @@ Date.prototype.minusMinutes = function (minutes) {
  * @param minutes 要增加的分钟数
  */
 Date.prototype.plusMinutes = function (minutes) {
-    return this.plusMillis(oneMinute * minutes);
+  return this.plusMillis(oneMinute * minutes);
 };
 
 /**
@@ -63,11 +63,11 @@ Date.prototype.plusMinutes = function (minutes) {
  *
  * @param millis 要减少的毫秒数
  */
-Date.prototype.minusMillis = function(millis) {
-    let time = this.getTime() - millis;
-    let newDate = new Date();
-    newDate.setTime(time);
-    return newDate;
+Date.prototype.minusMillis = function (millis) {
+  let time = this.getTime() - millis;
+  let newDate = new Date();
+  newDate.setTime(time);
+  return newDate;
 };
 
 /**
@@ -75,47 +75,59 @@ Date.prototype.minusMillis = function(millis) {
  *
  * @param millis 要增加的毫秒数
  */
-Date.prototype.plusMillis = function(millis) {
-    let time = this.getTime() + millis;
-    let newDate = new Date();
-    newDate.setTime(time);
-    return newDate;
+Date.prototype.plusMillis = function (millis) {
+  let time = this.getTime() + millis;
+  let newDate = new Date();
+  newDate.setTime(time);
+  return newDate;
 };
 
 /**
  * 设置时间为当天的 00:00:00.000
  */
 Date.prototype.setMinTime = function () {
-    this.setHours(0);
-    this.setMinutes(0);
-    this.setSeconds(0);
-    this.setMilliseconds(0);
-    return this;
+  this.setHours(0);
+  this.setMinutes(0);
+  this.setSeconds(0);
+  this.setMilliseconds(0);
+  return this;
 };
 
 /**
  * 设置时间为当天的 23:59:59.999
  */
 Date.prototype.setMaxTime = function () {
-    this.setHours(23);
-    this.setMinutes(59);
-    this.setSeconds(59);
-    this.setMilliseconds(999);
-    return this;
+  this.setHours(23);
+  this.setMinutes(59);
+  this.setSeconds(59);
+  this.setMilliseconds(999);
+  return this;
 };
 
 /**
  * 格式化日期
  */
 Date.prototype.formatDate = function () {
-    return this.getFullYear() + "-" + addZero(this.getMonth() + 1) + "-" + addZero(this.getDate());
+  return (
+    this.getFullYear() +
+    '-' +
+    addZero(this.getMonth() + 1) +
+    '-' +
+    addZero(this.getDate())
+  );
 };
 
 /**
  * 格式化时间
  */
 Date.prototype.formatTime = function () {
-    return addZero(this.getHours()) + ":" + addZero(this.getMinutes()) + ":" + addZero(this.getSeconds());
+  return (
+    addZero(this.getHours()) +
+    ':' +
+    addZero(this.getMinutes()) +
+    ':' +
+    addZero(this.getSeconds())
+  );
 };
 
 /**
@@ -124,24 +136,23 @@ Date.prototype.formatTime = function () {
  * @param split 日期和时间之间的分隔符，默认是一个空格
  */
 Date.prototype.formatDateTime = function (split = ' ') {
-    return this.formatDate() + split + this.formatTime();
+  return this.formatDate() + split + this.formatTime();
 };
 
 class DateUtil {
+  // 字符串转 Date 对象
+  static parseDate(str) {
+    return new Date(str.replace(/-/g, '/'));
+  }
 
-    // 字符串转 Date 对象
-    static parseDate(str) {
-        return new Date(str.replace(/-/g, '/'));
-    }
+  static formatMillis(millis) {
+    return moment(millis).format('YYYY-M-D H:m:s');
+  }
 
-    static formatMillis(millis) {
-        return moment(millis).format('YYYY-M-D H:m:s')
-    }
-
-    static firstDayOfMonth() {
-        const date = new Date();
-        date.setDate(1);
-        date.setMinTime();
-        return date;
-    }
+  static firstDayOfMonth() {
+    const date = new Date();
+    date.setDate(2);
+    date.setMinTime();
+    return date;
+  }
 }
